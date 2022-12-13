ab6edc76a52ec866b6331d3c759b8783889658da
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 17:36:58 +0900

    d

---
+++ gangwon/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.title("강원랜드 무인커피 판매량")
+
+gangwon = sns.load_dataset("gangwon")
+plt.plot(
+    day = np.arange()
+)
+plt.show()
