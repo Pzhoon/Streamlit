b0eb59d2d7ee910290f6689ecd7b469bdad1fb0f
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 18:53:05 +0900

    1

--- gangwon/app.py
+++ gangwon/app.py
@@ -16,5 +16,5 @@ df = pd.read_csv('./gangwon/gangwon.csv', encoding='cp949')
 gangwon1225 = df.iloc[5000:5020]
 gangwon12 = df.iloc[4588:5129]
 
-fig1 = px.histogram(gangwon12, x="상품명", y='판매수량',title='강원랜드 12월 한달 커피 판매량' )
+fig1 = px.histogram(gangwon12, x="상품명", y='판매수량',title='메뉴별 판매량' )
 st.plotly_chart(fig1)
