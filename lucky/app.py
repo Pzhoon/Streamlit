a5519345effdfab2589102b724c3edfe02eb0d22
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 14:21:14 +0900

    ㅇ

--- lucky/app.py
+++ lucky/app.py
@@ -11,9 +11,10 @@ st.image("https://images.unsplash.com/photo-1570303345338-e1f0eddf4946?ixlib=rb-
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
 
-tabs = st.tabs(["참가자"])
+tabs = st.tabs(["참가자", "조"])
 
-columns = st.columns(4)
+#0번째 탭에 컬럼(열)을 넣겠다
+columns = tabs[0].columns(4)
 # col1, col2, col3, col4
 for idx, col in enumerate(columns):
     # col.text_text_input(f"추첨 대상자 {idx+1}", key=idx)
