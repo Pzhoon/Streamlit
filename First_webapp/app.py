53c8a2a74ef1ceff3ec79b2cc5bfb55fb60d44a1
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 11:46:47 +0900

    Streamlit

---
+++ First_webapp/app.py
@@ -0,0 +1,14 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 제 첫 웹페이지 입니다.
+    * 1쿠쉬쿠쉬 = 1$
+    """
+    )
+
+st.image(
+    "https://images.unsplash.com/photo-1597019558926-3eef445fdf60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2thdGVib2FyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+)
