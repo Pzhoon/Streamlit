ef25fe194be0db6a75b43aa5d35f7e795bdc2d71
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 13:37:52 +0900

    d

--- lucky/app.py
+++ lucky/app.py
@@ -13,7 +13,11 @@ st.header("게임을 시작하지")
 columns - st.columns(4)
 # col1, col2, col3, col4
 for idx, col in enumerate(colums):
-    col.text_text_input
+    # col.text_text_input(f"추첨 대상자 {idx+1}", key=idx")
+    for idx2 in range(4):
+        # key가 겹치면 안 됨
+        col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"{idx+1 + idx2 *
+4}")
 st.text_input("추첨 대상자")
 # 13명이 소속될 조 이름을 넣을 위치
 
