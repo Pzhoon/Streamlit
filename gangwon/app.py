ecaf0f3dac57213707bca8d88582dc19e3bc332f
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 18:34:38 +0900

    d1

--- gangwon/app.py
+++ gangwon/app.py
@@ -3,11 +3,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
-plt.title("강원랜드 무인커피 판매량")
-
-gangwon = sns.load_dataset("gangwon")
-plt.plot(
-    day = np.arange()
+st.title(
+    """강원랜드 무인커피 판매량"""
+    )
+st.subheader(
+    ""12월 총 판매량""
 )
-plt.show()
+# gangwon = sns.load_dataset('gangwon')
+df = pd.read_csv('./gangwon/gangwon.csv')
+
+fig = plt.figure(figsize=(6, 3))
+gangwon['영업일자'] = 
