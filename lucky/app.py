946bbaed096d9b0bb14fa77c22b90b9bd4952151
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 12:38:48 +0900

    d

--- lucky/app.py
+++ lucky/app.py
@@ -8,6 +8,10 @@ st.header("게임을 시작하지")
 # https://docs.streamlit.io/library/cheatsheet
 
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
+# 3 x 4 (row, col)
+# 열을 배치하는 메소드
+columns - st.columns(4)
+# col1, col2, col3, col4
 st.text_input("추첨 대상자")
 # 13명이 소속될 조 이름을 넣을 위치
 
