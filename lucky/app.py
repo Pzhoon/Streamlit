485ab99bffadf5140d8bd631e4467e4eb4d5f231
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 15:07:49 +0900

    ㅇ

--- lucky/app.py
+++ lucky/app.py
@@ -32,6 +32,8 @@ for idx, col in enumerate(columns2):
     for idx2 in range(4):
         # key가 겹치면 안 됨
         col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"g{idx+1 + idx2 * 4}")
+#추첨버튼
+if st.button('추첨 시작'):
 ss = pd.Series(st.session_state)
 # st.write(ss)
 
@@ -39,8 +41,23 @@ ss2 = ss[ss.ne("")]
 st.write(ss2)
 n_idx = ss2.index.str.contains("n")
 n_data = ss2[n_idx]
-st.write(n_data)
+# st.write(n_data)
 
 g_idx - ss2.index.str.sontains("g")
-# <추첨 버튼>
+g_data = ss2[g_idx]
+# st.write(g_data)
+
+# n_data를 섞어줄 것임 (비복원으로)
+n_rd = np.random.choice(n_data, len(n_data), replace=False)
+# st.write(n_rd)
+g_rd = np.random.choice(g_data, len(g_data), replace=False)
+# st.write(g_rd)
+# 2. df 형태로 정리
 # 13개의 짝을 지어서 표시해줄 그래픽
+df = pd.DataFrame({
+    "추첨 대상자 이름": n_rd,
+    "조 이름": g_rd,
+})
+# st.balloons()
+st.snow()
+st.write(df)
