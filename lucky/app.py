db7de575ca50aa1fe7c3735660cae9b42a03e5f5
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 14:25:15 +0900

    d

--- lucky/app.py
+++ lucky/app.py
@@ -28,7 +28,7 @@ for idx, col in enumerate(columns2):
     # col.text_text_input(f"추첨 대상자 {idx+1}", key=idx)
     for idx2 in range(4):
         # key가 겹치면 안 됨
-        col.text_input(g"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"g{idx+1 + idx2 * 4}")
+        col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"g{idx+1 + idx2 * 4}")
 st.write(st.session_state)
 
 st.text_input("추첨 대상자")
