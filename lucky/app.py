90177bfe56d2afe996d63cc3f77537ac92c9ad03
Pzhoon <120434460+Pzhoon@users.noreply.github.com>
2022-12-13 14:29:03 +0900

    dd

--- lucky/app.py
+++ lucky/app.py
@@ -29,7 +29,8 @@ for idx, col in enumerate(columns2):
     for idx2 in range(4):
         # key가 겹치면 안 됨
         col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"g{idx+1 + idx2 * 4}")
-st.write(st.session_state)
+ss = pd.Series(st.session_state)
+st.write(ss)
 
 st.text_input("추첨 대상자")
 # 13명이 소속될 조 이름을 넣을 위치
